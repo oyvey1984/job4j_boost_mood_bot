4a98c3d8032ef95252c48a893f9fe7a73d8b75b3
Oy vey <oyiveyi@gmail.com>
2025-11-16 14:43:27 +0300

    @Profile | Пользователи системы [#505303]

---
+++ src/main/java/ru/job4j/model/User.java
@@ -0,0 +1,54 @@
+package ru.job4j.model;
+
+import java.util.Objects;
+
+public class User {
+
+    private long id;
+    private long clientId;
+    private long chatId;
+
+    public User(long id, long clientId, long chatId) {
+        this.id = id;
+        this.clientId = clientId;
+        this.chatId = chatId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(long clientId) {
+        this.clientId = clientId;
+    }
+
+    public long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(long chatId) {
+        this.chatId = chatId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
