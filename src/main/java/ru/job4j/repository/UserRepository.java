8f02bf38bd49ab2863a11a3917ec4a776e9ec4db
Oy vey <oyiveyi@gmail.com>
2025-11-19 08:09:54 +0300

    Модели данных [#505302]

--- src/main/java/ru/job4j/repository/UserRepository.java
+++ src/main/java/ru/job4j/repository/UserRepository.java
@@ -3,7 +3,6 @@ package ru.job4j.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.job4j.model.User;
-
 import java.util.List;
 
 @Repository
